eb4d0a4fc3f55573a56b1a3e5bbdf2068666a151
Keith Kikta <kkikta@gmail.com>
2016-11-10 12:11:37 -0600

    Updated to reflect changes in library. Amened variables and imports after testing in live environment

--- Usage.py
+++ Usage.py
@@ -1,16 +1,20 @@
-from JunkieFramework import Settings as jSet
-from JunkieFramework import JunkieFramework as jFrame
+import sys, os
+sys.path.append('%(script_path)s\custom' % {'script_path': fdmContext["SCRIPTSDIR"]})
+
+import JunkieFramework as jframe
+reload(jframe)
 
 #Usage Example
-settings = jSet(email_smtp_host="smtp.epmjunkie.com")
+settings = jframe.Settings(email_sender='fdmee@epmjunkie.com', email_smtp_host="smtp.epmjunkie.com")
+jf = jframe.Core(context=fdmContext, api=fdmAPI, settings=settings)
+
 
 def test():
-    t = jFrame(context=context, api=fdmAPI, settings=settings)
-    print(t.ImportFormat)
-    print(t.Period)
-    print(t.Period.Month)
-    print(t.Period.Year)
+    fdmAPI.logDebug(jf.ImportFormat)
+    fdmAPI.logDebug(jf.Period)
+    fdmAPI.logDebug(jf.Period.Month)
+    fdmAPI.logDebug(jf.Period.Year)
     attachments = []
     attachments.append(t.Email.create_attachment(path=r"c:\temp\test1.zip"))
     attachments.append(t.Email.create_attachment(path=r"c:\temp\test2.zip"))
-    t.Email.send(recipients="test@epmjunkie.com", subject="testing", attachments=attachments, body="Test Email")
+    jf.Email.send(recipients="test@epmjunkie.com", subject="testing", attachments=attachments, body="Test Email")
