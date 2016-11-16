4e07bef40ecfbb0eb28c46238759ec68bb4d782f
Keith Kikta <kkikta@gmail.com>
2016-11-16 17:32:02 -0600

    Convert from instance objects to properties. This might be easier for the average user in that it doesn't require casting to string or creating a method on _ContextGeneral to convert to string.

--- JunkieFramework.py
+++ JunkieFramework.py
@@ -13,40 +13,6 @@ class Settings(object):
 
 
 class Core(object):
-    class _ContextGeneral:
-        def __init__(self, context, value):
-            self.context = context
-            self.value = value
-
-        def __str__(self):
-            return str(self.context[self.value])
-
-    class _ContextPeriod:
-        class _Part:
-            def __init__(self, key, item=0):
-                self.key = str(key)
-                self.item = item
-                self.items = [None, None]
-                if self.key:
-                    import re
-                    self.items[0] = re.search('\D+', self.key).group(0)
-                    self.items[1] = re.search('\d+', self.key).group(0)
-
-            def __str__(self):
-                import re
-                items = []
-                return self.items[self.item].upper()
-
-        def __init__(self, context):
-            self.context = context
-            self.Month = self._Part(self, 0)
-            self.Year = self._Part(self, 1)
-
-        def __str__(self):
-            if "PERIODNAME" in self.context:
-                return self.context["PERIODNAME"]
-            return ""
-
     class _Email:
         def __init__(self, settings):
             self.sender = settings.email_sender
@@ -96,7 +62,7 @@ class Core(object):
             if tls:
                 client.ehlo()
                 client.starttls()
-                client.ehlo
+                client.ehlo()
             if password:
                 client.login(sender, password)
             message = MIMEMultipart()
@@ -144,42 +110,142 @@ class Core(object):
             return None
 
     def __init__(self, context, api, settings=Settings()):
-        self.context = context
-        self.api = api
+        self._context = context
+        self._api = api
         self.settings = settings
-        self.ApplicationID = self._ContextGeneral(context=context, value="APPID")
-        self.BatchScriptDirectory = self._ContextGeneral(context, value="BATCHSCRIPTDIR")
-        self.Category = self._ContextGeneral(context=context, value="CATNAME")
-        self.CategoryKey = self._ContextGeneral(context=context, value="CATKEY")
-        self.CheckStatus = self._ContextGeneral(context=context, value="CHKSTATUS")
-        self.EpmHome = self._ContextGeneral(context=context, value="EPMORACLEHOME")
-        self.EpmInstanceHome = self._ContextGeneral(context=context, value="EPMORACLEINSTANCEHOME")
-        self.ExportFlag = self._ContextGeneral(context=context, value="EXPORTFLAG")
-        self.ExportMode = self._ContextGeneral(context=context, value="EXPORTMODE")
-        self.ExportStatus = self._ContextGeneral(context=context, value="EXPSTATUS")
-        self.FileDirectory = self._ContextGeneral(context=context, value="FILEDIR")
-        self.FileName = self._ContextGeneral(context=context, value="FILENAME")
-        self.ImportFlag = self._ContextGeneral(context=context, value="IMPORTFLAG")
-        self.ImportFormat = self._ContextGeneral(context=context, value="IMPORTFORMAT")
-        # todo find out what IMPST is in the context.
-        self.ImportStatus = self._ContextGeneral(context=context, value="IMPSTATUS")
-        self.InboxDirectory = self._ContextGeneral(context=context, value="INBOXDIR")
-        self.LoadID = self._ContextGeneral(context=context, value="LOADID")
-        self.LocationKey = self._ContextGeneral(context=context, value="LOCKEY")
-        self.Location = self._ContextGeneral(context=context, value="LOCNAME")
-        self.MultiPeriodLoad = self._ContextGeneral(context=context, value="MULTIPERIODLOAD")
-        self.OutboxDirectory = self._ContextGeneral(context=context, value="OUTBOXDIR")
-        self.Period = self._ContextPeriod(context=context)
-        self.PeriodKey = self._ContextGeneral(context=context, value="PERIODKEY")
-        self.ProcessStatus = self._ContextGeneral(context=context, value="PROCESSSTATUS")
-        self.Rule = self._ContextGeneral(context=context, value="RULENAME")
-        self.RuleID = self._ContextGeneral(context=context, value="RULEID")
-        self.ScriptDirectory = self._ContextGeneral(context=context, value="SCRIPTSDIR")
-        self.SourceName = self._ContextGeneral(context=context, value="SOURCENAME")
-        self.SourceType = self._ContextGeneral(context=context, value="SOURCETYPE")
-        self.TargetApp = self._ContextGeneral(context=context, value="TARGETAPPNAME")
-        self.TargetAppDB = self._ContextGeneral(context=context, value="TARGETAPPDB")
-        self.TargetAppID = self._ContextGeneral(context=context, value="APPID")
-        self.ValidationStatus = self._ContextGeneral(context=context, value="VALSTATUS")
         self.Email = self._Email(settings=settings)
 
+    def getContextValue(self, name):
+        return str(self._context[name])
+
+    @property
+    def ApplicationID(self):
+        return self.getContextValue("APPID")
+
+    @property
+    def BatchScriptDirectory(self):
+        return self.getContextValue("BATCHSCRIPTDIR")
+
+    @property
+    def Category(self):
+        return self.getContextValue("CATNAME")
+
+    @property
+    def CategoryKey(self):
+        return self.getContextValue("CATKEY")
+
+    @property
+    def CheckStatus(self):
+        return self.getContextValue("CHKSTATUS")
+
+    @property
+    def EpmHome(self):
+        return self.getContextValue("EPMORACLEHOME")
+
+    @property
+    def EpmInstanceHome(self):
+        return self.getContextValue("EPMORACLEINSTANCEHOME")
+
+    @property
+    def ExportFlag(self):
+        return self.getContextValue("EXPORTFLAG")
+
+    @property
+    def ExportMode(self):
+        return self.getContextValue("EXPORTMODE")
+
+    @property
+    def ExportStatus(self):
+        return self.getContextValue("EXPSTATUS")
+
+    @property
+    def FileDirectory(self):
+        return self.getContextValue("FILEDIR")
+
+    @property
+    def FileName(self):
+        return self.getContextValue("FILENAME")
+
+    @property
+    def ImportFlag(self):
+        return self.getContextValue("IMPORTFLAG")
+
+    @property
+    def ImportFormat(self):
+        return self.getContextValue("IMPORTFORMAT")
+
+    @property
+    def ImportStatus(self):
+        return self.getContextValue("IMPSTATUS")
+
+    @property
+    def InboxDirectory(self):
+        return self.getContextValue("INBOXDIR")
+
+    @property
+    def LoadID(self):
+        return self.getContextValue("LOADID")
+
+    @property
+    def LocationKey(self):
+        return self.getContextValue("LOCKEY")
+
+    @property
+    def Location(self):
+        return self.getContextValue("LOCNAME")
+
+    @property
+    def MultiPeriodLoad(self):
+        return self.getContextValue("MULTIPERIODLOAD")
+
+    @property
+    def OutboxDirectory(self):
+        return self.getContextValue("OUTBOXDIR")
+
+    @property
+    def Period(self):
+        return self.getContextValue("PERIODNAME")
+
+    @property
+    def PeriodKey(self):
+        return self.getContextValue("PERIODKEY")
+
+    @property
+    def ProcessStatus(self):
+        return self.getContextValue("PROCESSSTATUS")
+
+    @property
+    def Rule(self):
+        return self.getContextValue("RULENAME")
+
+    @property
+    def RuleID(self):
+        return self.getContextValue("RULEID")
+
+    @property
+    def ScriptDirectory(self):
+        return self.getContextValue("SCRIPTSDIR")
+
+    @property
+    def SourceName(self):
+        return self.getContextValue("SOURCENAME")
+
+    @property
+    def SourceType(self):
+        return self.getContextValue("SOURCETYPE")
+
+    @property
+    def TargetApp(self):
+        return self.getContextValue("TARGETAPPNAME")
+
+    @property
+    def TargetAppDB(self):
+        return self.getContextValue("TARGETAPPDB")
+
+    @property
+    def TargetAppID(self):
+        return self.getContextValue("APPID")
+
+    @property
+    def ValidationStatus(self):
+        return self.getContextValue("VALSTATUS")
