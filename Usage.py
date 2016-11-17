7c8f37b6db082dc673f8431e84e920719087371b
Keith Kikta <kkikta@gmail.com>
2016-11-17 15:31:52 -0600

    Removed month and period as they are no longer available.

--- Usage.py
+++ Usage.py
@@ -12,8 +12,6 @@ jf = jframe.Core(context=fdmContext, api=fdmAPI, settings=settings)
 def test():
     fdmAPI.logDebug(jf.ImportFormat)
     fdmAPI.logDebug(jf.Period)
-    fdmAPI.logDebug(jf.Period.Month)
-    fdmAPI.logDebug(jf.Period.Year)
     attachments = []
     attachments.append(jf.Email.create_attachment(path=r"c:\temp\test1.zip", name="test1.zip"))
     attachments.append(jf.Email.create_attachment(path=r"c:\temp\test2.zip", name="test2.zip"))
